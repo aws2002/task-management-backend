ed2cf6485ac0d4835a6f60d3a16b1517dd06a534
Osama Husam <osamahusam848@gmail.com>
2022-09-04 14:37:30 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {Logger} from "@nestjs/common"
+import { Logger } from "@nestjs/common"
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
-  const logger=new Logger('bootstrap')
+  const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule);
-  const port=3000
+  const config = new DocumentBuilder()
+    .setTitle('Task management')
+    .setDescription('The task-management API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  const port = 3000
   await app.listen(port);
   logger.log(`App listening at port ${port}`)
 }
